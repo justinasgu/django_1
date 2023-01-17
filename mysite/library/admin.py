76d16a50773419bdf3e0c8b7f334208a675e851c
justinasgu <guzys.justinas@gmail.com>
2023-01-17 17:46:16 +0200

    pakoregavome bookinstance atvaizdavima admin puslapyje

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -7,7 +7,16 @@ from .models import Author,Genre,Book,BookInstance
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'isbn', 'display_genre')
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
