b9593934f7293a460a74cb0113cf5f2d640bc605
justinasgu <guzys.justinas@gmail.com>
2023-01-17 16:29:40 +0200

    aprasyti modeliu pavadinimai vienaskaita ir daugiskaita

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -6,6 +6,13 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(verbose_name='Pavadinimas', max_length=200, help_text='Iveskite knygos zanra, pvz "detektyvas"')
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Zanras'
+        verbose_name_plural = 'Zanrai'
+
 class Book(models.Model):
     title = models.CharField(verbose_name='Pavadinimas', max_length=200)
     summary = models.TextField(verbose_name='Aprasymas', max_length=1000)
@@ -13,9 +20,23 @@ class Book(models.Model):
     author = models.ForeignKey(to='Author', on_delete=models.SET_NULL, null=True)
     genre = models.ManyToManyField(to='Genre')
 
+    def __str__(self):
+        return f'"{self.title}" {self.author}'
+
+    class Meta:
+        verbose_name = 'Knyga'
+        verbose_name_plural = 'Knygos'
+
 class Author(models.Model):
     first_name = models.CharField(verbose_name='Vardas', max_length=100)
     last_name = models.CharField(verbose_name='Pavarde', max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Autorius'
+        verbose_name_plural = 'Autoriai'
 
 class BookInstance(models.Model):
     book = models.ForeignKey(to='Book', on_delete=models.CASCADE)
@@ -36,3 +57,10 @@ class BookInstance(models.Model):
         default='a',
         help_text='Statusas',
     )
+
+    def __str__(self):
+        return  f'{self.book} - {self.uuid}'
+
+    class Meta:
+        verbose_name = 'Uzsakyta preke'
+        verbose_name_plural = 'Uzsakytos prekes'
