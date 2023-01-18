1e91235a4c5f553c4c62dbf8803eddf28ad6b609
justinasgu <guzys.justinas@gmail.com>
2023-01-18 23:53:40 +0200

    pilnai padaryta django3 uzduotys

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, BookInstance, Author
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Labas pasauli')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return  render(request, 'index.html', context=context)
